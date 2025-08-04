9a5affb489751ac95d852be299f59fe8a93eae6c
Bertonsin <gustavobertonsin@hotmail.com>
2025-08-03 22:24:56 -0300

    feat: Added ingredient repository

---
+++ src/main/kotlin/org/example/orderme/repositories/IngredientRepository.kt
@@ -0,0 +1,9 @@
+package org.example.orderme.repositories
+
+import org.example.orderme.entities.Ingredient
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface IngredientRepository : JpaRepository<Ingredient, Long> {
+}
